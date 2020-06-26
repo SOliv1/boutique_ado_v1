5a8667b13563bef31909e4a616836eef4fe91316
Sara Oliver <s.oliver1@icloud.com>
2020-06-26 23:00:11 +0000

    fix merge conflict

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,8 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.profile, name='profile'),
-    path('order_history/<order_number>',
-         views.order_history,
-         name='order_history'),
+    path('', views.profile, name='profile')
 ]

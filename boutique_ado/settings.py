5219e888a1a04b95308e3894a009da544f5ea09e
Sara Oliver <s.oliver1@icloud.com>
2020-07-02 13:09:06 +0000

    updated

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -10,8 +10,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import dj_database_url
 
-import env
 if os.path.exists("env.py"):
+    import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))

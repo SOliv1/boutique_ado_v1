e5534745c9f21a7e5828d2248e2ae46bbcae2cfb
Sara Oliver <s.oliver1@icloud.com>
2020-07-03 12:34:34 +0000

    fix static root settings file path

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -130,8 +130,8 @@ else:
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
 
 
 # Password validation
@@ -168,6 +168,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)

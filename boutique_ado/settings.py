feef3b74c7c950a9ac33cc8f02ae9a728421a430
Sara Oliver <s.oliver1@icloud.com>
2020-07-03 15:01:03 +0000

    aws s3 object parameters add

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -178,6 +178,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
+if 'USE_AWS' in os.environ:
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'sjk-boutiqueado'
     AWS_S3_REGION_NAME = 'us-east-1'

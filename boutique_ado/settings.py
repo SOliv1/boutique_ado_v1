79c6a54e916f4a967c6af3d00a5760adee93b2d5
Sara Oliver <s.oliver1@icloud.com>
2020-07-07 13:59:19 +0000

    modify data again to deploy to heroku

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -1,8 +1,8 @@
 import os
 import dj_database_url
 
-#   if os.path.exists("env.py"):
-#       import env
+if os.path.exists("env.py"):
+    import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))

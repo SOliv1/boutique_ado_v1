c3cb0d5b60e7cbff916148e514df65d5b84d6152
Sara Oliver <s.oliver1@icloud.com>
2020-08-18 19:43:40 +0000

    new creditials

--- products/views.py
+++ products/views.py
@@ -43,12 +43,11 @@ def all_products(request):
             query = request.GET['q']
             if not query:
                 messages.error(request,
-                               "You didn't enter any search criteria!")
+                    "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
-
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
-
+    
     current_sorting = f'{sort}_{direction}'
 
     context = {
